db3389e4136b6105834a72165a2a3113f0f58ba0
niklas-hjelm <niklas.hjelm@iths.se>
2023-12-12 22:19:03 +0100

    Update with working client

---
+++ Clients/MegaQuiz/MegaQuiz/MegaQuiz/Program.cs
@@ -0,0 +1,33 @@
+using MegaQuiz.Client.Pages;
+using MegaQuiz.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHttpClient("gateway", c => c.BaseAddress = new Uri("http://quizgateway:8080"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
